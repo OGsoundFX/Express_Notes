27b02f0085cc3860f5e5599c05adc92df17331cd
Olivier Girardot <olivier.girardot@axonetiq.com>
2021-03-16 18:29:42 +0100

    added a get and a post route, and body-parser

--- Basic_Express_Config/index.js
+++ Basic_Express_Config/index.js
@@ -1,8 +1,9 @@
 const express = require('express');     // requiring express after "npm i express"
 const app = express();                  // setting up the express app
+const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 8080;
-// Setting port to 8080 locally or whatever PORT has been se to.
+const port = process.env.PORT || 8081;
+// Setting port to 8081 locally or whatever PORT has been se to.
 // In production the PORT will be whatever your server uses (like Heroku or something else)
 
 app.set('view engine', 'ejs');
@@ -13,10 +14,29 @@ app.use('/assets', express.static(__dirname + '/public'));
 // setting our static folder taht we named "public".
 // The http get request "/assets" will look into the "public" folder.
 
-app.get('/', (req, res) => {
-    res.render('index');
-    console.log(`listening on port ${port}`);
-});
-// simple get request that renders the "index.ejs" page in the view folder
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.listen(port); // tells express to listen to the indicated port (8080 locally)
+    // DEFINING ROUTES
+    // 1- GET:
+    app.get('/', (req, res) => {
+        res.render('index', { test: 'Type in your name:' });
+        // renders the index.ejs page in views. This is an ejs feature.
+        // also passes some javascript values that can be displayed in the view (test).
+    });
+
+    app.get('/hello', (req, res) => {
+        res.send('<html><head><title>Testing page</title></head><body><h1>Hello World!</h1></body></html>');
+        // sends the html code to the path
+    });
+    
+    // 2- POST:
+    app.post('/postpath', urlencodedParser, (req, res) => {
+        // res.send(`Thank you ${req.body.name}`);
+        res.render('response', { name: req.body.name })
+    });
+    // 2 ways of displaying the result of a post request
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+// tells express to listen to the indicated port (8080 locally)
